d9702e80cc2b6fad57376c9aeeed760363eaec18
AjaySingh <ajay.singh11@ibm.com>
2026-06-22 23:00:34 +0530

    feature-3 api key added

--- src/main/java/com/eteration/dp/solid/di/bad/PrinterDesk.java
+++ src/main/java/com/eteration/dp/solid/di/bad/PrinterDesk.java
@@ -10,6 +10,11 @@ public class PrinterDesk {
 	
 	
 	public void printFilefast(){
+
+		String API_KEY = "q342342304u2038";
+
+		System.out.println(API_TOKEN);
+
 		printer.print("File Content printed");
 	}
 	
